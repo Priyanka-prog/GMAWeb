0a569aa80b49bd1e82579b78851574a8d87143d2
Priyanka Rajendran <56512263+Priyanka-prog@users.noreply.github.com>
2021-08-19 14:25:50 +0200

    changes in checklogin and deletion, inspection page

--- src/it/polimi/gma/controller/CheckLogin.java
+++ src/it/polimi/gma/controller/CheckLogin.java
@@ -1,91 +1,85 @@
 package it.polimi.gma.controller;
 
 import java.io.IOException;
-
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+	
+import org.apache.commons.lang.StringEscapeUtils;
 import javax.ejb.EJB;
-import javax.servlet.ServletContext;
+import javax.persistence.NonUniqueResultException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringEscapeUtils;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-
-import it.polimi.gma.services.UserService;
 import it.polimi.gma.entities.User;
 import it.polimi.gma.exceptions.CredentialsException;
-import javax.persistence.NonUniqueResultException;
+import it.polimi.gma.services.UserService;
+
 
 @WebServlet("/CheckLogin")
 public class CheckLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private TemplateEngine templateEngine;
+       
 	@EJB(name = "it.polimi.gma.services/UserService")
 	private UserService usrService;
-
-	public CheckLogin() {
-		super();
-	}
-
-	public void init() throws ServletException {
-		ServletContext servletContext = getServletContext();
-		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
-		templateResolver.setTemplateMode(TemplateMode.HTML);
-		this.templateEngine = new TemplateEngine();
-		this.templateEngine.setTemplateResolver(templateResolver);
-		templateResolver.setSuffix(".html");
-	}
-
+	
+    public CheckLogin() {
+        super();
+    }
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// obtain and escape params
-		String usrn = null;
+		String usr = null;
 		String pwd = null;
+		
 		try {
-			usrn = StringEscapeUtils.escapeJava(request.getParameter("username"));
+			usr = StringEscapeUtils.escapeJava(request.getParameter("username"));
 			pwd = StringEscapeUtils.escapeJava(request.getParameter("pwd"));
-			if (usrn == null || pwd == null || usrn.isEmpty() || pwd.isEmpty()) {
+			if (usr == null || pwd == null || usr.isEmpty() || pwd.isEmpty()) {
 				throw new Exception("Missing or empty credential value");
 			}
 
 		} catch (Exception e) {
-			// for debugging only e.printStackTrace();
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing credential value");
 			return;
 		}
+		
 		User user;
 		try {
-			// query db to authenticate for user
-			user = usrService.checkCredentials(usrn, pwd);
+			user = usrService.checkCredentials(usr, pwd);
 		} catch (CredentialsException | NonUniqueResultException e) {
-			e.printStackTrace();
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Could not check credentials");
-			return;
+			return;	
 		}
-
-		// If the user exists, add info to the session and go to home page, otherwise
-		// show login page with error message
-
+		
 		String path;
 		if (user == null) {
-			ServletContext servletContext = getServletContext();
-			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-			ctx.setVariable("errorMsg", "Incorrect username or password");
-			path = "/index.html";
-			templateEngine.process(path, ctx, response.getWriter());
-		} else {
+			PrintWriter out = response.getWriter();
+			out.println("<p>Incorrect username or password</p>");
+		} 
+		else {
 			request.getSession().setAttribute("user", user);
-			path = getServletContext().getContextPath() + "/Home";
+			Date date = new Date(); 
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+			user.setLastLogin(formatter.format(date));
+			try {
+				usrService.updateUser(user);
+			} catch (Exception e) {
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Timestamp update error");
+				return;
+			}
+			if (user.getRole().equals("Admin")) {
+				path = getServletContext().getContextPath() + "/AdminPage";
+			}
+			else {
+				path = getServletContext().getContextPath() + "/Home";
+			}
 			response.sendRedirect(path);
 		}
-
 	}
-
-	public void destroy() {
-	}
+	
+	public void destroy() {}
 }
